2d8f582e1a23f5c6f20f41f8e227f4aec5e6bb96
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-05-03 02:10:14 -0300

    feat: create layout and header components and styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer } from "./styles";
+import { NavLink } from "react-router-dom";
+
+import { Timer, Scroll } from "phosphor-react";
+
+import logo from "../../assets/logo.svg";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+
+        <NavLink to="/history" title="History">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+}
