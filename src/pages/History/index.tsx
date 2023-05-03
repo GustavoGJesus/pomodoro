713acc03859242727f726b46623a174514c7bc89
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-05-03 03:31:12 -0300

    feat: created status component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -21,31 +21,25 @@ export function History() {
               <td>Tarefa 01</td>
               <td>20 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa 01</td>
               <td>20 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa 01</td>
               <td>20 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa 01</td>
-              <td>20 minutos</td>
-              <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa 01</td>
-              <td>20 minutos</td>
-              <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
